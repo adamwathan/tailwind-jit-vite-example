d0d66d95eee239798bb38ed1f224e41725eac50b
Adam Wathan <adam.wathan@gmail.com>
2021-03-17 15:56:03 -0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-6 py-12">
+    <HelloWorld name="Ricardo Cooper" />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<style scoped>
+.foo {
+}
+</style>
